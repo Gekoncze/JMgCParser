a5b7a6c9d334e7c4170311e0bbd8bfb93f957b91
Mg <gekoncze@seznam.cz>
2023-11-05 20:17:08 +0100

    extracted function list parser from function parser

--- src/cz/mg/c/parser/services/entity/FunctionParser.java
+++ src/cz/mg/c/parser/services/entity/FunctionParser.java
@@ -4,11 +4,9 @@ import cz.mg.annotations.classes.Service;
 import cz.mg.annotations.requirement.Mandatory;
 import cz.mg.c.parser.components.TokenReader;
 import cz.mg.c.parser.entities.Function;
-import cz.mg.c.parser.entities.Variable;
 import cz.mg.c.parser.entities.brackets.CurlyBrackets;
 import cz.mg.c.parser.entities.brackets.RoundBrackets;
 import cz.mg.c.parser.services.CMainEntityParser;
-import cz.mg.c.parser.services.ListParser;
 import cz.mg.collections.list.List;
 import cz.mg.tokenizer.entities.Token;
 
@@ -21,9 +19,8 @@ public @Service class FunctionParser implements CMainEntityParser {
                 if (instance == null) {
                     instance = new FunctionParser();
                     instance.typeParser = TypeParser.getInstance();
-                    instance.variableParser = VariableParser.getInstance();
-                    instance.listParser = ListParser.getInstance();
                     instance.nameParser = NameParser.getInstance();
+                    instance.variableListParser = VariableListParser.getInstance();
                 }
             }
         }
@@ -31,9 +28,8 @@ public @Service class FunctionParser implements CMainEntityParser {
     }
 
     private @Service TypeParser typeParser;
-    private @Service VariableParser variableParser;
-    private @Service ListParser listParser;
     private @Service NameParser nameParser;
+    private @Service VariableListParser variableListParser;
 
     private FunctionParser() {
     }
@@ -43,28 +39,11 @@ public @Service class FunctionParser implements CMainEntityParser {
         Function function = new Function();
         function.setOutput(typeParser.parse(reader));
         function.setName(nameParser.parse(reader));
-        function.setInput(readInput(reader.read(RoundBrackets.class)));
+        function.setInput(variableListParser.parse(reader.read(RoundBrackets.class)));
         if (reader.has(CurlyBrackets.class)) {
             function.setImplementation(readImplementation(reader.read(CurlyBrackets.class)));
         }
         return function;
-    }
-
-    private @Mandatory List<Variable> readInput(@Mandatory RoundBrackets brackets) {
-        List<List<Token>> parameters = listParser.parse(new TokenReader(brackets.getTokens()));
-        List<Variable> input = new List<>();
-        if (hasParameters(parameters)) {
-            for (List<Token> parameter : parameters) {
-                TokenReader reader = new TokenReader(parameter);
-                input.addLast(variableParser.parse(reader));
-                reader.readEnd();
-            }
-        }
-        return input;
-    }
-
-    private boolean hasParameters(@Mandatory List<List<Token>> parameters) {
-        return !(parameters.count() == 1 && parameters.getFirst().count() == 0);
     }
 
     private @Mandatory List<Token> readImplementation(@Mandatory CurlyBrackets brackets) {
