ebc4f64cee666beab01bd64c947746ba1da85a7f
Mg <gekoncze@seznam.cz>
2024-03-29 08:29:27 +0100

    rename exception

--- src/cz/mg/c/parser/exceptions/ParseException.java
+++ src/cz/mg/c/parser/exceptions/ParseException.java
@@ -2,9 +2,9 @@ package cz.mg.c.parser.exceptions;
 
 import cz.mg.annotations.classes.Error;
 import cz.mg.annotations.requirement.Mandatory;
-import cz.mg.tokenizer.exceptions.CodeException;
+import cz.mg.tokenizer.exceptions.TraceableException;
 
-public @Error class ParseException extends CodeException {
+public @Error class ParseException extends TraceableException {
     public ParseException(int position, @Mandatory String message) {
         super(position, message);
     }
