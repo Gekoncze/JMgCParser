818c1345924f0c286f9512c6a18e0fa39cb30fb6
mg <gekoncze@seznam.cz>
2024-08-18 13:48:55 +0200

    fixed enum type parser

--- src/cz/mg/c/parser/services/entity/type/EnumTypeParser.java
+++ src/cz/mg/c/parser/services/entity/type/EnumTypeParser.java
@@ -2,12 +2,10 @@ package cz.mg.c.parser.services.entity.type;
 
 import cz.mg.annotations.classes.Service;
 import cz.mg.annotations.requirement.Mandatory;
-import cz.mg.c.entities.CModifier;
+import cz.mg.c.entities.types.CDataType;
 import cz.mg.c.parser.components.TokenReader;
 import cz.mg.c.entities.types.CType;
 import cz.mg.c.parser.services.entity.EnumParser;
-import cz.mg.collections.set.Set;
-import cz.mg.collections.set.Sets;
 
 public @Service class EnumTypeParser implements InlineTypeParser {
     private static volatile @Service EnumTypeParser instance;
@@ -20,6 +18,7 @@ public @Service class EnumTypeParser implements InlineTypeParser {
                     instance.modifiersParser = ModifiersParser.getInstance();
                     instance.pointerTypeParser = PointerTypeParser.getInstance();
                     instance.enumParser = EnumParser.getInstance();
+                    instance.typeConnector = TypeConnector.getInstance();
                 }
             }
         }
@@ -29,17 +28,17 @@ public @Service class EnumTypeParser implements InlineTypeParser {
     private @Service ModifiersParser modifiersParser;
     private @Service PointerTypeParser pointerTypeParser;
     private @Service EnumParser enumParser;
+    private @Service TypeConnector typeConnector;
 
     private EnumTypeParser() {
     }
 
     @Override
     public @Mandatory CType parse(@Mandatory TokenReader reader) {
-        CType type = new CType();
-        Set<CModifier> modifiers = modifiersParser.parse(reader);
-        type.setTypename(enumParser.parse(reader));
-        type.setModifiers(Sets.union(modifiers, modifiersParser.parse(reader)));
-        type.setPointers(pointerTypeParser.parse(reader));
-        return type;
+        CDataType dataType = new CDataType();
+        dataType.getModifiers().setCollection(modifiersParser.parse(reader));
+        dataType.setTypename(enumParser.parse(reader));
+        dataType.getModifiers().setCollection(modifiersParser.parse(reader));
+        return typeConnector.connect(pointerTypeParser.parse(reader), dataType);
     }
 }
