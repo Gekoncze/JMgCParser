0c878a70a1dd0bf70a4006eb64d93d4ab1516e40
mg <gekoncze@seznam.cz>
2024-08-18 13:19:38 +0200

    fixed type parser

--- src/cz/mg/c/parser/services/entity/TypeParser.java
+++ src/cz/mg/c/parser/services/entity/TypeParser.java
@@ -4,13 +4,13 @@ import cz.mg.annotations.classes.Service;
 import cz.mg.annotations.requirement.Mandatory;
 import cz.mg.c.entities.CModifier;
 import cz.mg.c.entities.CTypename;
+import cz.mg.c.entities.types.CDataType;
+import cz.mg.c.entities.types.CPointerType;
 import cz.mg.c.entities.types.CType;
 import cz.mg.c.parser.components.TokenReader;
 import cz.mg.c.parser.services.CEntityParser;
-import cz.mg.c.parser.services.entity.type.FunctionTypeParser;
-import cz.mg.c.parser.services.entity.type.InlineTypeParsers;
-import cz.mg.c.parser.services.entity.type.ModifiersParser;
-import cz.mg.c.parser.services.entity.type.PointerTypeParser;
+import cz.mg.c.parser.services.entity.type.*;
+import cz.mg.collections.pair.Pair;
 import cz.mg.collections.set.Set;
 import cz.mg.collections.set.Sets;
 import cz.mg.token.tokens.WordToken;
@@ -26,6 +26,7 @@ public @Service class TypeParser implements CEntityParser {
                 instance.pointerTypeParser = PointerTypeParser.getInstance();
                 instance.inlineTypeParsers = InlineTypeParsers.getInstance();
                 instance.functionTypeParser = FunctionTypeParser.getInstance();
+                instance.typeConnector = TypeConnector.getInstance();
             }
         }
         return instance;
@@ -35,13 +36,14 @@ public @Service class TypeParser implements CEntityParser {
     private @Service PointerTypeParser pointerTypeParser;
     private @Service InlineTypeParsers inlineTypeParsers;
     private @Service FunctionTypeParser functionTypeParser;
+    private @Service TypeConnector typeConnector;
 
     @Override
     public @Mandatory CType parse(@Mandatory TokenReader reader) {
         Set<CModifier> modifiers = modifiersParser.parse(reader);
         CType type = inlineTypeParsers.parse(reader, modifiers);
         if (type == null) {
-            type = parsePlainType(reader, modifiers);
+            type = parseNamedType(reader, modifiers);
             if (reader.has(functionTypeParser::matches)) {
                 type = functionTypeParser.parse(reader, type);
             }
@@ -49,11 +51,12 @@ public @Service class TypeParser implements CEntityParser {
         return type;
     }
 
-    private @Mandatory CType parsePlainType(@Mandatory TokenReader reader, @Mandatory Set<CModifier> modifiers) {
-        CType type = new CType();
-        type.setTypename(new CTypename(reader.read(WordToken.class).getText()));
-        type.setModifiers(Sets.union(modifiers, modifiersParser.parse(reader)));
-        type.setPointers(pointerTypeParser.parse(reader));
-        return type;
+    private @Mandatory CType parseNamedType(@Mandatory TokenReader reader, @Mandatory Set<CModifier> modifiers) {
+        CDataType dataType = new CDataType();
+        dataType.setTypename(new CTypename(reader.read(WordToken.class).getText()));
+        dataType.setModifiers(Sets.union(modifiers, modifiersParser.parse(reader)));
+
+        Pair<CPointerType, CPointerType> pointerTypes = pointerTypeParser.parse(reader);
+        return typeConnector.connect(pointerTypes, dataType);
     }
 }
